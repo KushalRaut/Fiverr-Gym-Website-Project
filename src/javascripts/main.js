b4752939398631f9ed22ad400bf66e31b1b9ca23
Kushal Raut <kushalraut5@gmail.com>
2022-04-22 12:57:32 +0545

    Implementing Local Storage

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -38,15 +38,17 @@ const offers = [
   },
 ];
 
+localStorage.setItem('offers', JSON.stringify(offers));
+
 const [offer1, offer2, offer3] = [
   document.getElementById('offer-title-1'),
   document.getElementById('offer-title-2'),
   document.getElementById('offer-title-3'),
 ];
 
-offer1.innerHTML = offers[0].title;
-offer2.innerHTML = offers[1].title;
-offer3.innerHTML = offers[2].title;
+offer1.innerHTML = JSON.parse(localStorage.getItem('offers'))[0].title;
+offer2.innerHTML = JSON.parse(localStorage.getItem('offers'))[1].title;
+offer3.innerHTML = JSON.parse(localStorage.getItem('offers'))[2].title;
 
 const [offerDes1, offerDes2, offerDes3] = [
   document.getElementById('offer-desp-1'),
@@ -54,9 +56,9 @@ const [offerDes1, offerDes2, offerDes3] = [
   document.getElementById('offer-desp-3'),
 ];
 
-offerDes1.innerHTML = offers[0].despcription;
-offerDes2.innerHTML = offers[1].despcription;
-offerDes3.innerHTML = offers[2].despcription;
+offerDes1.innerHTML = JSON.parse(localStorage.getItem('offers'))[0].despcription;
+offerDes2.innerHTML = JSON.parse(localStorage.getItem('offers'))[1].despcription;
+offerDes3.innerHTML = JSON.parse(localStorage.getItem('offers'))[2].despcription;
 
 const about = {
   name: 'JOHNNY SMITH',
@@ -65,12 +67,15 @@ const about = {
     'Hello my name is Johnny and I am a gym instructor from California,Los Angeles. I am a certified trainer and have been helping people change their life with exercise from the last decade. Fit Nation has been my home for last 5 years and I am excited to help you transform yourself and your life with the help of my personal training.',
 };
 
+localStorage.setItem('about', JSON.stringify(about));
+
+
 const [trainerName, trainerTitle, trainerDesp] = [
   document.getElementById('about-name'),
   document.getElementById('about-title'),
   document.getElementById('about-desp'),
 ];
 
-trainerName.innerHTML = about.name;
-trainerTitle.innerHTML = about.position;
-trainerDesp.innerHTML = about.despcription;
+trainerName.innerHTML = JSON.parse(localStorage.getItem('about')).name;
+trainerTitle.innerHTML = JSON.parse(localStorage.getItem('about')).position;
+trainerDesp.innerHTML = JSON.parse(localStorage.getItem('about')).despcription;
